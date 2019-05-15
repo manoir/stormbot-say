175a93c37c6d87ec01e8b683a82f08731b14fc0d
Paul Fariello <paul@fariello.eu>
2019-05-15 22:40:30 +0200

    Bump version to 1.7.dev2 (peering)

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='stormbot-say',
-      version='1.2.1',
+      version='1.7.dev2',
       description='text to speech plugin for stormbot',
       long_description=long_description,
       author='Paul Fariello',
