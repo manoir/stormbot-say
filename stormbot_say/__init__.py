0ac605a8d9502d8d14d4b4f290e657298c82201e
SABATIER Boris <boris.sabatier@stormshield.eu>
2019-05-29 16:46:41 +0200

    Better args naming

--- stormbot_say/__init__.py
+++ stormbot_say/__init__.py
@@ -21,16 +21,16 @@ class Say(Plugin):
         subparser.add_argument("--lang", type=str, default=self.lang, help="Say lang (default: %(default)s)")
         subparser.add_argument("text", type=str, help="Text to say")
 
-    async def run(self, msg, parser, args, peer):
-        if peer is None:
+    async def run(self, raw_msg, parser, args, origin_peer):
+        if origin_peer is None:
             for peer in self._bot.get_peers(self):
-                self._bot.peer_forward_msg(self, peer, msg)
+                self._bot.peer_forward_msg(self, peer, raw_msg)
 
         tts = gTTS(text=args.text, lang=args.lang)
         with NamedTemporaryFile() as f:
             tts.write_to_fp(f)
             f.flush()
             cmd = ['play', '-t', 'mp3', f.name]
-            if not peer:
+            if not origin_peer:
                 self._bot.write(args.text)
             subprocess.check_call(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
