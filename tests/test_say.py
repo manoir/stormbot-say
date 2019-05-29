fcea349da083919aab1ffd85f1c0f11ab0243dd0
SABATIER Boris <boris.sabatier@stormshield.eu>
2019-05-29 16:47:19 +0200

    Add test for run

--- tests/test_say.py
+++ tests/test_say.py
@@ -1,8 +1,6 @@
 from unittest import TestCase, mock
 import asyncio
 
-
-
 from stormbot_say import Say
 
 def _run(coro):
@@ -32,18 +30,51 @@ class TestSayCommand(TestCase):
         # Then
         mock_arg_parse.add_parser.assert_called()
 
-    def test_run(self):
+    @mock.patch('stormbot_say.subprocess')
+    @mock.patch('stormbot_say.gTTS')
+    def test_normal_run(self, mock_tts, mock_subprocess):
         # Given
         mock_bot = mock.Mock()
+        mock_bot.get_peers.return_value = [mock.Mock(), mock.Mock()]
         mock_args = mock.Mock()
+
         mock_parser = mock.Mock()
         mock_parsed_args = mock.Mock()
-        mock_peer = mock.Mock()
         plugin = Say(mock_bot, mock_args)
-        msg = "Message to say"
+        raw_msg = "hello"
 
         # When
-        _run(plugin.run(msg, mock_parser, mock_parsed_args, mock_peer))
+        _run(plugin.run(raw_msg, mock_parser, mock_parsed_args, None))
 
         # Then
-        #mock_arg_parse.add_parser.assert_called()
+        for mock_peer in mock_bot.get_peers.return_value:
+            mock_bot.peer_forward_msg.assert_any_call(plugin, mock_peer, raw_msg)
+
+        mock_tts.assert_called_with(lang=mock_parsed_args.lang, text=mock_parsed_args.text)
+        mock_tts.return_value.write_to_fp.assert_called()
+        mock_subprocess.check_call.assert_called()
+        mock_bot.write.assert_called_with(mock_parsed_args.text)
+
+    @mock.patch('stormbot_say.subprocess')
+    @mock.patch('stormbot_say.gTTS')
+    def test_run_from_peer(self, mock_tts, mock_subprocess):
+        # Given
+        mock_bot = mock.Mock()
+        mock_bot.get_peers.return_value = [mock.Mock(), mock.Mock()]
+        mock_args = mock.Mock()
+
+        mock_parser = mock.Mock()
+        mock_parsed_args = mock.Mock()
+        plugin = Say(mock_bot, mock_args)
+        raw_msg = "hello"
+
+        # When
+        _run(plugin.run(raw_msg, mock_parser, mock_parsed_args, "peer_id"))
+
+        # Then
+        mock_bot.peer_forward_msg.assert_not_called()
+
+        mock_tts.assert_called_with(lang=mock_parsed_args.lang, text=mock_parsed_args.text)
+        mock_tts.return_value.write_to_fp.assert_called()
+        mock_subprocess.check_call.assert_called()
+        mock_bot.write.assert_not_called()
